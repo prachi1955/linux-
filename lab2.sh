17a9a927eb7f3ec279b9a953b33dd5f9f4bf96b4
prachi1955 <148520678+prachi1955@users.noreply.github.com>
2023-10-27 10:08:01 +0530

    Add files via upload

---
+++ lab2.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+#example
+greeting=hello
+name=prachi-padodara
+echo $greeting
+echo $name 
---
+++ lab3.sh
@@ -0,0 +1,13 @@
+function cal(){
+var1=5
+var2=10
+((add=var1+var2))
+((sub=var2-var1))
+((mul=var1*var2))
+((div=var2/var1))
+}
+cal
+echo $add
+echo $sub
+echo $mul
+echo $div
